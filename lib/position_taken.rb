6f3329fac874f5c71034c72f1d8ecf477ff2d261
Alie Stone <helloaliestone@gmail.com>
2020-09-08 18:48:12 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,8 @@
 def position_taken?(board, position)
   if board[position] == "" || " " || "X" || "O"
     true
-  else board[position] == nil
+  else
+    board[position] == nil
     false
   end
 end
